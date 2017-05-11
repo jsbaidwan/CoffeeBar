5157237173084706424221a52861671afcaa38d4
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-05-11 23:56:51 +0530

    change price label to order summary

--- app/src/main/java/com/example/jaspreetsingh/coffeebar/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/coffeebar/MainActivity.java
@@ -78,12 +78,9 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
     private void displayText(String message)    {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
