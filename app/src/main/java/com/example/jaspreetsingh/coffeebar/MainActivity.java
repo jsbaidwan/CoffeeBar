459f69751229928827681abcde44c5e6af72cecf
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-05-31 23:44:55 +0530

    cleaning the project

--- app/src/main/java/com/example/jaspreetsingh/coffeebar/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/coffeebar/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText(""+ number);
     }
 
     private void displayText(String message)    {
